2bcbed73444d3db74896ba2da32e62520c3437a1
yuanfang000205 <2296787770@qq.com>
2021-11-04 15:25:15 +0800

    简单的用户管理界面

--- src/views/user/UserList.vue
+++ src/views/user/UserList.vue
@@ -1,21 +1,56 @@
 <template>
-  <div>
-    用户管理
+  <div class="header">
+      用户管理
   </div>
+  <div class="user-select">
+    <user-select/>
+  </div>
+  <div class="addU">
+    <add-user/>
+  </div>
+  <div class="userinfo">
+    <user-info/>
+  </div>
+
 </template>
 
-<script>
+<script >
+import AddUser from './AddUser.vue'
+import UserInfo from './UserInfo.vue'
+import UserSelect from './UserSelect.vue'
 export default {
-  components: {},
-
+  components: {
+    AddUser,
+    UserInfo,
+    UserSelect
+  },
   data () {
-    return {}
+    return {
+    }
   },
 
   computed: {},
 
-  methods: {}
+  methods: {
+  }
 }
 </script>
 <style lang='less'>
+.user-select {
+  width: auto;
+  height: 60px;
+  background-color: rgb(167, 212, 212);
+  text-align: center;
+  line-height: 60px;
+  border-radius: 5px;
+}
+.el-input {
+  width: 400px;
+}
+.addU {
+  float: left;
+  margin-top: 10px;
+  margin-bottom: 20px;
+}
+
 </style>
