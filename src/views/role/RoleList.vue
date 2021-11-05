2a56b1ad41dc001cb796ae483d3038a877958487
李田杰 <894316189@qq.com>
2021-11-05 14:10:47 +0800

    用户权限加了一点点代码

--- src/views/role/RoleList.vue
+++ src/views/role/RoleList.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>权限管理</div>
+  <div>
+    <el-container>
+      <el-header>
+        <el-form :inline="true"
+                 :model="formInline"
+                 class="demo-form-inline">
+          <el-form-item label="用户名">
+            <el-input v-model="formInline.user"
+                      placeholder="请输入用户名"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary"
+                       @click="onSubmit">查询</el-button>
+          </el-form-item>
+        </el-form>
+      </el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </div>
 </template>
 
 <script>
@@ -7,7 +25,12 @@ export default {
   components: {},
 
   data () {
-    return {}
+    return {
+      formInline: {
+        user: '',
+        region: ''
+      }
+    }
   },
 
   computed: {},
